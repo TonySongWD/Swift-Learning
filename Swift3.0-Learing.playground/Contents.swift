7223497cb898076e2c5c8411c77f4f71d3ea6efa
TonySongWD <tonysong072@gmail.com>
2017-08-04 10:50:01 +0800

    Complete swift review.

--- Swift3.0-Learing.playground/Contents.swift
+++ Swift3.0-Learing.playground/Contents.swift
@@ -1017,7 +1017,34 @@ class Counter {
     }
 }
 
+class ThreeSource: NSObject, CounterDataSource {
+    let fixedIncrement: Int = 3
+}
+var counter = Counter()
+counter.dataSource = ThreeSource()
+for _ in 1...4 {
+    counter.increment()
+    print(counter.count)
+}
 
+@objc class TowardsZeroSource: NSObject, CounterDataSource {
+    func incrementForCount(count: Int) -> Int {
+        if count == 0 {
+            return 0
+        } else if count < 0 {
+            return 1
+        } else {
+            return -1
+        }
+    }
+}
+
+counter.count = -4
+counter.dataSource = TowardsZeroSource()
+for _ in 1...5 {
+    counter.increment()
+    print(counter.count)
+}
 
 
 
